c2f14a640e53a1f289baa4c06b3fbeb16c6f7d0b
David Moranchel <david.arcshtag@gmail.com>
2016-05-02 21:00:45 -0500

    Add new project of command line for learning dart

--- web_app_projects/hello/web/main.dart
+++ web_app_projects/hello/web/main.dart
@@ -3,8 +3,24 @@
 
 import 'dart:html';
 
+InputElement toDoInput;
+UListElement toDoList;
+
 
 void main() {
   //Hello word
   querySelector('#output').text = 'Hello Word, this is my first web app with Dart.';
+
+  //Create list
+  toDoInput = querySelector('#to-do-input');
+  toDoList = querySelector('#to-do-list');
+  toDoInput.onChange.listen(addToDoItem);
+  document.onInput("Hi beach");
+}
+
+void addToDoItem(Event e) {
+  var newToDo = new LIElement();
+  newToDo.text = toDoInput.value;
+  toDoInput.value = '';
+  toDoList.children.add(newToDo);
 }
